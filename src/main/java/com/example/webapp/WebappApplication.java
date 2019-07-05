34fc51f9aa8a59eaa44594dbf49207d9ae3c347d
lig <lig@senthink.com>
2019-07-05 12:51:40 +0800

    修改启动类

--- src/main/java/com/example/webapp/WebappApplication.java
+++ src/main/java/com/example/webapp/WebappApplication.java
@@ -2,6 +2,7 @@ package com.example.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContextAware;
 
@@ -10,6 +11,11 @@ public class WebappApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(WebappApplication.class, args);
+    }
+
+    @Override
+    public SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebappApplication.class);
     }
 
 }
