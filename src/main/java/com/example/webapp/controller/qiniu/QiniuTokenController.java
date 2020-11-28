3c2d9ed6e2d9c682af91c604d4cf0d50bf5777c7
lig <lig@senthink.com>
2020-11-28 16:44:11 +0800

    fix: 添加千牛获取token

---
+++ src/main/java/com/example/webapp/controller/qiniu/QiniuTokenController.java
@@ -0,0 +1,32 @@
+package com.example.webapp.controller.qiniu;
+
+import com.example.webapp.response.Response;
+import com.example.webapp.service.QiniuService;
+import com.qiniu.api.auth.AuthException;
+import org.json.JSONException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ligen
+ * @title: QiniuTokenController
+ * @projectName webapp
+ * @description:
+ * @date 2020/11/2816:35
+ */
+
+@RestController
+public class QiniuTokenController {
+
+    @Autowired
+    private QiniuService qiniuService;
+
+
+    @GetMapping("/qiniu/getToken")
+    public Response getToken() throws AuthException, JSONException {
+        return new Response<>(Response.CODE_SUCCESS, qiniuService.getToken());
+    }
+
+
+}
---
+++ src/main/java/com/example/webapp/service/QiniuService.java
@@ -0,0 +1,32 @@
+package com.example.webapp.service;
+
+import com.qiniu.api.auth.AuthException;
+import com.qiniu.api.auth.digest.Mac;
+import com.qiniu.api.config.Config;
+import com.qiniu.api.rs.PutPolicy;
+import org.json.JSONException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ligen
+ * @title: QiniuService
+ * @projectName webapp
+ * @description:
+ * @date 2020/11/2816:36
+ */
+@Service
+public class QiniuService {
+
+
+    public String getToken() throws AuthException, JSONException {
+
+        Config.ACCESS_KEY = "WNpck9WBdrlus0mXhUYSeEVUxGewRJLWWDHcp4o1";
+        Config.SECRET_KEY = "hVByGzSINwRNtHaegyezRa-K-QR_PtU1-aOKiM7V";
+        Mac mac = new Mac(Config.ACCESS_KEY, Config.SECRET_KEY);
+        // 请确保该bucket已经存在
+        String bucketName = "testlglglg";
+        PutPolicy putPolicy = new PutPolicy(bucketName);
+        return putPolicy.token(mac);
+    }
+
+}
